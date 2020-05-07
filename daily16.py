d7eddc83fc6c98585427302fa1f3fbe03c00550d
rjrobert <jhrobert@pm.me>
2020-05-07 11:16:10 -0700

    day 16

---
+++ daily16.py
@@ -0,0 +1,23 @@
+"""
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Twitter.
+
+You run an e-commerce website and want to record the last N order ids in a log. Implement a data structure to accomplish this, with the following API:
+
+record(order_id): adds the order_id to the log
+get_last(i): gets the ith last element from the log. i is guaranteed to be smaller than or equal to N.
+You should be as efficient with time and space as possible.
+"""
+from collections import deque
+
+
+class Log:
+    def __init__(self, length):
+        self.orders = deque('', length)
+
+    def record(self, order_id):
+        self.orders.append(order_id)
+
+    def get_last(self, i):
+        return self.orders[i]
